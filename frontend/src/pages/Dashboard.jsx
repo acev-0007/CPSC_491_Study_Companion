3db613aaa409ac2b84ae47622cb8c93a1e7c407d
acev-0007 <acevedoanthony514@gmail.com>
2026-09-15 16:34:48 -0700

    added link button on dashboard that goes to flashcards page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <Link to="/flashcards">Study Flashcards</Link>
+    </div>
+  );
 }
 
 export default Dashboard;
