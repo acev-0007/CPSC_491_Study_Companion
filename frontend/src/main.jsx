7f91a702697ae6da7648a3a87c32c9385b88029b
Madou Moore <Madoumoore@gmail.com>
2026-09-25 12:36:10 -0700

    refactor(styles): add shared design system and global styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./index.css";
+import "./styles/variables.css";
+import "./styles/globals.css";
+import "./styles/auth.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
